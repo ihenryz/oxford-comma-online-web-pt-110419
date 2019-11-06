4478790cc120d8ada4a7627a2b01f37b06c38517
Henry Phan <h.phan06@gmail.com>
2019-11-06 05:30:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+
+def oxford_comma(array)
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    prepend_array = array[-1].prepend("and ")
+    array.join(", ")
+  else array.length > 3 
+    prepend_array = array[-1].prepend("and ")
+    array.join(", ")
+  end 
 end
