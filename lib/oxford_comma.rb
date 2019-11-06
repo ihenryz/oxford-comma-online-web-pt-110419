98560f4365d9b0cbad20a377f868caa3b6619eae
Henry Phan <h.phan06@gmail.com>
2019-11-06 05:09:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
     elsif array.length == 3 
-    new_array = array[2].prepend("and ")
+    binding.pry
     
   end 
 end
