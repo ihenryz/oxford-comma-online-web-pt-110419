3228174c08b9eface3ccbd8c84e872133f51d5d2
Henry Phan <h.phan06@gmail.com>
2019-11-06 03:40:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,11 @@ require 'pry'
 
 
 def oxford_comma(array)
+  if array[1] == true
+    array.split(" and ")
+  else array.join
   # array.join
   # array.each {|n| puts n}
   # binding.pry
+  end
 end
